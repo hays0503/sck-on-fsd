dc4b76df49bfec8e9e216174d2c167c01dce83f8
hays0503 <cthtufgbv@mail.ru>
2024-12-30 18:10:00 +0500

    fix env

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,30 @@
 import { locales } from "@/i18n/routing";
-import { ApiUrl } from "@/shared/api/url";
+import { ApiUrl, UrlApiWithDomain, UrlRevalidate } from "@/shared/api/url";
+import { iCity } from "@/shared/types/city";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const url = process.env.HOST_URL ?? ApiUrl;
-  return locales.map((locale) => ({
-    url: `${url}/${locale}/main`,
-    lastModified: new Date(),
-  }));
+  try {
+    const response = await fetch(UrlApiWithDomain.getCity, {
+      ...UrlRevalidate.getCity,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Ошибка HTTP: ${response.status}`);
+    }
+
+    const data: iCity[] = await response.json();
+
+    const url = process.env.HOST_URL ?? ApiUrl;
+
+    return locales.flatMap((locale) =>
+      data.map((city) => ({
+        url: `${url}/${locale}/city/${city.additional_data.EN}/main`,
+        lastModified: new Date(),
+      }))
+    );
+  } catch (error) {
+    console.error("Ошибка при создании sitemap:", error);
+    return [];
+  }
 }
